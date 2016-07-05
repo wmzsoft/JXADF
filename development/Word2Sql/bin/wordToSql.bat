4278d680d0fb98c623c4ed8ea947cdf8f65fc6c2
wmzsoft <wmzsoft@gmail.com>
2016-07-05 22:20:15 +0800

    --

--- development/Word2Sql/bin/wordToSql.bat
+++ development/Word2Sql/bin/wordToSql.bat
@@ -14,6 +14,6 @@
 @set jxclasspath=..\lib\log4j-1.2.8.jar;..\lib\jacob.jar;..\lib\jdom.jar;..\lib\ojdbc14.jar;.;..\lib\jacob.dll;..\lib\antlr-2.7.7.jar;..\lib\asm-4.0.jar;..\lib\commons-attributes-api.jar;..\lib\commons-attributes-compiler.jar;..\lib\commons-logging.jar;..\lib\groovy-2.1.5.jar;..\lib\spring-beans.jar;..\lib\spring-context.jar;..\lib\spring-core.jar
 @set JAVA_OPTION = -Dsiteid=JX -Dorgid=JXKJ -Dstoragepartition=MAXIMO_DATA -DstoragepartitionIndex=MAXIMO_INDEX
 @SHIFT
-@%JAVA_HOME%\bin\java -classpath "%jxclasspath%" -jar %JAVA_OPTION% WordToSql-2013.jar %0=%1 %2=%3 %4=%5 %6=%7 %8=%9 
+@"%JAVA_HOME%\bin\java" -classpath "%jxclasspath%" -jar %JAVA_OPTION% WordToSql-2013.jar %0=%1 %2=%3 %4=%5 %6=%7 %8=%9 
 @pause
 @echo on
--- development/Word2Sql/test/test.bat
+++ development/Word2Sql/test/test.bat
@@ -1,3 +1,3 @@
 @call env.bat
-@WordToSql impfile=%WORD2SQL_HOME%\test\doc\DDR20120412-请假管理详细设计说明书.doc expfile=%WORD2SQL_HOME%\test\sql\leave.sql framework=J
+@WordToSql impfile="%WORD2SQL_HOME%\test\doc\DDR20120412-请假管理详细设计说明书.doc" expfile="%WORD2SQL_HOME%\test\sql\leave.sql" framework=J
 @cd ..
